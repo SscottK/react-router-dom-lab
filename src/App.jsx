688b801ae6064c113237c5a2adbe59bdbfc511b1
ScottKinnear <gr8whtninjassk@gmail.com>
2024-09-29 14:10:05 -0700

    Built NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-// src/App.jsx
+import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
+// import MailBoxForm from './components/MailboxForm'
+// import MailBoxList from './components/MailboxList'
+// import MailBoxDetails from './components/MailboxDetails'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <div>      
+      <h2>Hello World</h2>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+
+
+
+    return (
+        <div className="navbar">
+            <Link to="/"><h2>Home</h2></Link>
+            <Link to="/mailboxes"><h2>Mailboxes</h2></Link>
+            <Link to="/new-mailbox"><h2>New Mailbox</h2></Link>
+        </div>
+    )
+}
+
+export default NavBar
